9c370b32be5111bb2af24eaa5f96a9d48e2b4634
kvokka-in-space <107798904+kvokka-in-space@users.noreply.github.com>
2024-09-25 23:11:15 +0300

    Create Error.php
    Add Error class

---
+++ src/Bot/Model/Entity/Message/Error.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace RetailCrm\Mg\Bot\Model\Entity\Message;
+
+use JMS\Serializer\Annotation as Serializer;
+use JMS\Serializer\Annotation\Accessor;
+use JMS\Serializer\Annotation\SkipWhenEmpty;
+use JMS\Serializer\Annotation\Type;
+use RetailCrm\Mg\Bot\Model\ModelInterface;
+
+class Error implements ModelInterface
+{
+    /**
+     * @var int
+     *
+     * @Type("integer")
+     */
+    private $code;
+
+    /**
+     * @var string
+     *
+     * @Type("string")
+     */
+    private $message;
+
+    public function getCode(): int
+    {
+        return $this->code;
+    }
+
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+}
